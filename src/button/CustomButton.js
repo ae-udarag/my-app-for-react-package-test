dda06f62c82dd7d01c5ab37e7cdd781a1112a7da
G.D Udara Lahiru Sampath <udara.g@aeturnum.com>
2021-08-26 10:29:18 +0530

    button modifications

---
+++ src/button/CustomButton.js
@@ -0,0 +1,12 @@
+// import Button from '@material-ui/core/Button';
+
+import React from "react";
+
+export const CustomButton = ({ text }) => {
+    return (
+        // <Button variant="contained" color="primary">
+        //     {props.text}
+        // </Button>
+        <span>{text}</span>
+    );
+}
